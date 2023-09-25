2dab1b1556e98b3626b81a8d5eef1bcd278a1af6
Noah Lett <noahblett96@gmail.com>
2023-09-25 12:11:00 -0700

    Added effect hooks to manage the verification of password match and regex checks

--- src/Register.js
+++ src/Register.js
@@ -22,9 +22,32 @@ const Register = () => {
     const [validMatch, setValidMatch] = useState(false);
     const [matchFocus, setMatchFocus] = useState(false);
 
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user);
+        console.log(result);
+        console.log(user);
+        setValidName(result);
+    }, [user])
+
+    useEffect(() => {
+        const result = PWD_REGEX.test(pwd);
+        console.log(result);
+        console.log(pwd);
+        setValidPwd(result);
+        const match = pwd === matchPwd;
+        setValidMatch(match);
+    }, [pwd. matchPwd])
+
     return (
         <div>
-
+            
         </div>
     )
 }
