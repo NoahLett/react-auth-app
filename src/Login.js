cbf8bf035abf411cdd3b09b91c0d6727286b1696
Noah Lett <noahblett96@gmail.com>
2023-09-30 13:27:40 -0700

    Created initial Login component and added states

---
+++ src/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useRef, useState, useEffect } from "react";
+
+const Login = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Login
