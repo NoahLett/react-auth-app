a70f2d728dc5cc8536247823e716515ae5d31812
Noah Lett <noahblett96@gmail.com>
2023-10-04 16:42:05 -0700

    Created layout component and set up react router to handle paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Register from './Register';
 import Login from './Login';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <main className='App'>
-      <Login />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public Routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* Protected Routes */}
+      </Route>
+    </Routes>
   );
 }
 
