d3423c2ea9220c10ce25b8529037c5408919db53
Noah Lett <noahblett96@gmail.com>
2023-10-22 13:29:13 -0700

    Fixed editor and admin pages

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 
 const Editor = () => {
-    <div>
-        <h1>Editor's Page</h1>
-        <br/>
-        <p>You must have an editor role to be here.</p>
+    return (
         <div>
-            <Link to="/">Home</Link>
+            <h1>Editor's Page</h1>
+            <br/>
+            <p>You must have an editor role to be here.</p>
+            <div>
+                <Link to="/">Home</Link>
+            </div>
         </div>
-    </div>
+    )
 }
 
 export default Editor;
