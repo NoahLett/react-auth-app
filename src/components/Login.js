456ed3ae8e23cd5cebc258a621462df1f5d33313
Noah Lett <noahblett96@gmail.com>
2023-11-30 16:04:58 -0800

    Added persist state and corresponding jsx to login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ import { Link, useNavigate, useLocation } from "react-router-dom";
 const LOGIN_URL = '/auth';
 
 const Login = () => {
-    const { setAuth } = useAuth();
+    const { setAuth, persist, setPersist } = useAuth();
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || '/';
@@ -83,6 +83,15 @@ const Login = () => {
                 required />
 
                 <button>Sign In</button>
+                <div className="persisCheck">
+                    <input 
+                        type="checkbox"
+                        id="persist"
+                        onChange={togglePersist}
+                        checked={persist}
+                    />
+                    <label htmlFor="persist">Trust This Device</label>
+                </div>
             </form>
             <p>
                 Need an Account?&nbsp;
